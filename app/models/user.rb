e1a5f49188ce5af7fc4615a52244b303b58a102a
Yancy Zhang <yuanxi.zhang@gmail.com>
2019-02-02 22:52:08 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,4 @@
 class User < ActiveRecord::Base
-<<<<<<< HEAD
 	has_secure_password
 	validates_presence_of :username, :password
-=======
-  has_secure_password
->>>>>>> 70f00bc473e4b713ae8589eff3fb58c0fb814a75
 end
